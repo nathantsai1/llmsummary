5ab2d8299019571a741149f543f7c9313a3aca12
Nathan t <tsai.nathan.2009@gmail.com>
2024-08-17 21:45:20 -0700

    prompts p5

--- helpers.py
+++ helpers.py
@@ -1,3 +1,12 @@
 def temp():
     # doesn't do anything
     return 1
+
+def college(essay):
+    with open('/txt/college/college0.txt', 'r') as file:
+        # Read the entire content of the file
+        content = file.read()
+    content += essay
+    with open('/txt/college/college0.txt', 'r') as file:
+        # Read the entire content of the file
+        content += file.read()
