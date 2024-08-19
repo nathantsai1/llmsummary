e3bf33c0f94180e53ea6259a4d3f7b8309ded911
Nathan t <tsai.nathan.2009@gmail.com>
2024-08-18 22:58:36 -0700

    reading p3

--- app.py
+++ app.py
@@ -37,7 +37,36 @@ def check():
         model = genai.GenerativeModel('gemini-1.5-flash')
         # take prompt
         response = model.generate_content(colleged)
-        return response.text
+
+        # get information from json
+        alread = reads(response)
+        hi = alread
+        jsoned = json.loads(hi)
+
+        # and sort it
+        # by reviews(numbers)
+        j1 = jsoned["1"][0]
+        j2 = jsoned["2"][0]
+        j3 = jsoned["3"][0]
+        j4 = jsoned["4"][0]
+
+        # and strings
+        js1 = jsoned["1"]
+        js2 = jsoned["2"]
+        js3 = jsoned["3"]
+        js4 = jsoned["4"]
+        for i in range(0, 3):
+            # deletes number
+            js1 = js1 - js1[0]
+            js2 = js2 - js2[0]
+            js3 = js3 - js3[0]
+            js4 = js4 - js4[0]
+        # make json(1 = string, 1r = rating)
+        dictionary = {"1": js1, "1r": j1, "2": js2, "2r": j2, "3": js3, "3r": j3, "4": js4, "4r": j4} 
+        mjsn = json.dumps(dictionary)
+
+        return render_template('done.html', json=mjsn, response=alread[1])
+        # # for the record  - 1 = hook, 2 = description of hook 3 = how the author changed 4 = amount of discriptions
     else:
         # with open('no.txt', 'r') as file:
         #     # Read the entire content of the file
